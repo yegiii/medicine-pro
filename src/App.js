aca1f1122c59a428564e848323beda94d6fab01f
DESKTOP-M90QUJH\yegan <yeganeh.b1379@gmail.com>
2023-04-30 18:10:42 +0430

    files & folders created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
-}
-
+};
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./header.css";
+import Medicine from "../../assets/images/Medicine.svg";
+import { AiOutlineSearch } from "react-icons/ai";
+
+function Header() {
+  return <div>Header</div>;
+}
+
+export default Header;
