1295058e382fbfbf8de0a0efffdd83eb0779d709
DESKTOP-M90QUJH\yegan <yeganeh.b1379@gmail.com>
2023-05-04 16:05:19 +0430

    account page added

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -5,7 +5,28 @@ import "./account.css";
 function Account() {
   return (
     <>
-      <h1>account</h1>
+      <section className="accountInfo">
+        <div className="container boxItems">
+          <h1>Account Information</h1>
+          <div className="content">
+            <div className="left">
+              <div className="img flexCenter">
+                <input type="file" accept="image/*" src={image} alt="imgs" />
+                <img src={image} alt="" className="image-preview" />
+              </div>
+            </div>
+            <div className="right">
+              <label>Username</label>
+              <input type="text" required />
+              <label>Email</label>
+              <input type="text" required />
+              <label>Password *</label>
+              <input type="text" required />
+              <button className="button">Update</button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
