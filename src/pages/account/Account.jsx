03cb8fe5a880efb5a1bf1c60954ea7ecf7a2cd58
DESKTOP-M90QUJH\yegan <yeganeh.b1379@gmail.com>
2023-05-04 15:49:22 +0430

    account.css added

---
+++ src/pages/account/Account.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import image from "../../assets/images/input.png";
+import "./account.css";
+
+function Account() {
+  return (
+    <>
+      <h1>account</h1>
+    </>
+  );
+}
+
+export default Account;
