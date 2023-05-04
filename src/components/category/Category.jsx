1e233e3cb5ae078c78784f4e30fa5ae1128da140
DESKTOP-M90QUJH\yegan <yeganeh.b1379@gmail.com>
2023-05-04 12:07:47 +0430

    breakpoint repaired

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -30,6 +30,21 @@ function Category() {
           slidesToScroll: 2,
         },
       },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
     ],
   };
 
