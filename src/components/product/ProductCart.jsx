9ee8f2ad0d15a4d6aab3b7d6d8fe73cc39307768
DESKTOP-M90QUJH\yegan <yeganeh.b1379@gmail.com>
2023-05-09 12:54:35 +0430

    a product added to store

--- src/components/product/ProductCart.jsx
+++ src/components/product/ProductCart.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { AiOutlinePlusCircle } from "react-icons/ai";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { cartActions } from "../../store/cartSlice";
 
-function ProductCart(props) {
+function ProductCart({ myKey, id, cover, name, price }) {
+  const dispatch = useDispatch();
+
+  const addToCart = () => {
+    dispatch(cartActions.addToCart({ id, name, price, cover }));
+  };
   return (
     <>
       <div className="box boxItems" id="product">
         <div className="img">
-          <Link>
-            <img src={props.cover} alt="cover" />
+          <Link to="/#">
+            <img src={cover} alt="cover" />
           </Link>
         </div>
 
         <div className="details">
-          <h3>${props.price}</h3>
-          <p>{props.name}</p>
-          <button>
+          <h3>${price}</h3>
+          <p>{name}</p>
+          <button onClick={addToCart}>
             <AiOutlinePlusCircle />
           </button>
         </div>
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -2,9 +2,36 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
   name: "cart",
-  initialState: [],
-  totalQuantity: 0,
-  reducers: {},
+  initialState: {
+    itemsList: [],
+    totalQuantity: 0,
+  },
+
+  reducers: {
+    addToCart(state, action) {
+      const newItem = action.payload;
+
+      //check item is already exists
+      const existsItem = state.itemsList.find((item) => item.id === newItem.id);
+
+      if (existsItem) {
+        existsItem.quantity++;
+        existsItem.totalPrice += newItem.price;
+      } else {
+        state.itemsList.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.name,
+          cover: newItem.cover,
+        });
+
+        state.totalQuantity++;
+      }
+    },
+  },
 });
 
+export const cartActions = cartSlice.actions;
 export default cartSlice;
