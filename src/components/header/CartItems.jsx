21f0b7362279bb93d193e31c5946c5af5741c3fd
DESKTOP-M90QUJH\yegan <yeganeh.b1379@gmail.com>
2023-05-09 13:56:23 +0430

    plus button and minus button connect to store

--- src/components/header/CartItems.jsx
+++ src/components/header/CartItems.jsx
@@ -1,33 +1,42 @@
 import React from "react";
 import { AiOutlineClose, AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cartSlice";
 
-function CartItems(props) {
+function CartItems({ id, cover, name, price, quantity, totalPrice }) {
+  const dispatch = useDispatch();
+  const incCartitems = () => {
+    dispatch(cartActions.addToCart({ id, name, price }));
+  };
+  const descCartitems = () => {
+    dispatch(cartActions.removeFromCart(id));
+  };
   return (
     <>
       <div className="cardList">
         <div className="cartContent">
           <div className="img">
-            <img src={props.cover} alt="product-1" />
+            <img src={cover} alt="product-1" />
             <button className="remove flexCenter">
               <AiOutlineClose />
             </button>
           </div>
           <div className="details">
-            <p>{props.name}</p>
-            <label htmlFor="">Unit Price ${props.price}</label>
+            <p>{name}</p>
+            <label htmlFor="">Unit Price ${price}</label>
 
             <div className="price">
               <div className="qty flexCenter">
-                <button className="plus">
+                <button className="plus" onClick={incCartitems}>
                   <AiOutlinePlus />
                 </button>
-                <button className="num">1{props.quantity}</button>
-                <button className="minus">
+                <button className="num">{quantity}</button>
+                <button className="minus" onClick={descCartitems}>
                   <AiOutlineMinus />
                 </button>
               </div>
 
-              <div className="priceTitle">${props.totalPrice}</div>
+              <div className="priceTitle">${totalPrice}</div>
             </div>
           </div>
         </div>
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -30,6 +30,17 @@ const cartSlice = createSlice({
         state.totalQuantity++;
       }
     },
+    removeFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.itemsList.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.itemsList = state.itemsList.filter((item) => item.id !== id);
+        state.totalQuantity--;
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
+    },
   },
 });
 
